b620a061f3de9e45b481e57eb6ffc7dbb68632a8
Asha <hampana@Hampana.local>
2024-02-18 21:51:17 +0530

    test

---
+++ set02/problem05.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+int input();
+int find_gcd(int a, int b);
+void output(int a, int b, int gcd);
+int main()
+ {
+    int a, b,gcd;
+    a = input();
+    b = input();
+    gcd = find_gcd(a,b);     
+    output(a,b,gcd);
+    return 0;
+}
+
+int input() {
+    int a;
+    printf("Enter the number:");
+    scanf("%d",&a);
+    return a;
+}
+int find_gcd(int a, int b)
+ {
+int gcd;
+    for (int i =1; i <= a && i <= b; i++) {
+        if (a % i == 0 && b % i == 0) {
+            gcd = i; 
+        }
+    }
+ return gcd;
+}
+void output(int a, int b, int gcd) 
+{
+    printf("The gcd of %d and %d is %d\n", a, b, gcd);
+}
