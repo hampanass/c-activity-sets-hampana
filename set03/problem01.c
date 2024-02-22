e88ecc8e29b2ee6ea076a9599644774a1abee25e
Asha <hampana@Hampana.local>
2024-02-22 22:59:06 +0530

    test

---
+++ set03/problem01.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include<math.h>
+void input(float *x1, float *y1, float *x2, float *y2);
+float find_distance(float x1, float y1, float x2, float y2);
+void output(float x1, float y1, float x2, float y2, float distance);
+
+int main()
+{
+    float x1,x2,y1,y2,distance;
+    input(&x1,&x2,&y1,&y2);
+    distance = find_distance(x1,x2,y1,y2);
+    output(x1,x2,y1,y2,distance);
+    return 0;
+}
+
+void input(float *x1, float *y1, float *x2, float *y2)
+{
+    printf("Enter the first x-cordinate:\n");
+    scanf("%f",x1);
+    printf("Enter the second x-cordinate:\n");
+    scanf("%f",x2);
+    printf("Enter the first y-cordinate:\n");
+    scanf("%f",y1);
+    printf("Enter the second y-cordinate:\n");
+    scanf("%f",y2);
+}
+
+float find_distance(float x1, float y1, float x2, float y2)
+{
+    float distance;
+    distance = sqrt(pow(x2-x1,2) + pow(y2-y1,2));
+    return distance;
+}
+
+void output(float x1, float y1, float x2, float y2, float distance)
+{
+  printf("distance between teh pionts A(%f,%f) and B(%f,%f) is %f",x1,x2,y1,y2,distance);
+}
