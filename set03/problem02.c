25385d8a5f66339f8aefb3ac1d6f90ce154c5b06
Asha <hampana@Hampana.local>
2024-02-23 13:48:41 +0530

    test

---
+++ set03/problem02.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <math.h>
+void input_triangle(float *x1, float *y1, float *x2, float *y2, float *x3, float *y3);
+int is_triangle(float x1, float y1, float x2, float y2,float x3, float y3);
+void output(float x1, float y1, float x2, float y2,float x3, float y3, int result);
+
+int main()
+{
+    float x1,x2,y1,y2
+}
