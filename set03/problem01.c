cb233957064b67d8c2fee73b74412ada994dedfa
Asha <hampana@Hampana.local>
2024-02-24 23:22:10 +0530

    test

--- set03/problem01.c
+++ set03/problem01.c
@@ -34,5 +34,5 @@ float find_distance(float x1, float y1, float x2, float y2)
 
 void output(float x1, float y1, float x2, float y2, float distance)
 {
-  printf("distance between teh pionts A(%f,%f) and B(%f,%f) is %f",x1,x2,y1,y2,distance);
+  printf("distance between the pionts A(%f,%f) and B(%f,%f) is %f",x1,x2,y1,y2,distance);
 }
--- set03/problem02.c
+++ set03/problem02.c
@@ -6,5 +6,9 @@ void output(float x1, float y1, float x2, float y2,float x3, float y3, int resul
 
 int main()
 {
-    float x1,x2,y1,y2
+    float x1,x2,x3,y1,y2,y3;
+    input_triangle(&x1,&x2,&x3,&y1,&y2,&y3);
+    int result = is_triangle(x1,x2,x3,y1,y2,y3);
+    output(x1,x2,x3,y1,y2,y3,result);
+    return 0;
 }
