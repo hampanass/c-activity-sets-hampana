9a6c7f34c8f9a0d152f39d4396a87b2669c4d917
Asha <hampana@Hampana.local>
2024-02-29 20:44:48 +0530

    test

--- set03/problem06.c
+++ set03/problem06.c
@@ -2,3 +2,12 @@
 void input_string(char* a, char* b);
 int sub_str_index(char* string, char* substring);
 void output(char *string, char *substring, int index);
+
+int main()
+{
+    char a[100],b[100];
+    input_string(a,b);
+    int index = sub_str_index(a,b);
+    output(a,b,index);
+    return 0;
+}
