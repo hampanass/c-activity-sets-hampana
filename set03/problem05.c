35d5d8c13fcb1eb34f217b2f402db4d162bc06fc
Asha <hampana@Hampana.local>
2024-03-02 21:02:02 +0530

    test

--- set03/problem05.c
+++ set03/problem05.c
@@ -1,51 +1,43 @@
-#include <stdio.h>
+// Write a program to find all the prime numbers between 2 to n Eratosthenes Sieve method.
+#include<stdio.h>
 int input_array_size();
-void init_array(int n, int a[n]);
-void erotosthenes_sieve(int n, int a[n]);
-void output(int n, int a[n]);
+void init_array(int n, int a[]);
+void erotosthenes_sieve(int n, int a[]);
+void output(int n, int a[]);
 
-int main()
-{
+int input_array_size() {
+  int n;
+    printf("Enter the size of the array:");
+    scanf("%d",&n);
+    return n;
+}
+void init_array(int n, int a[]) {
+    for(int i = 2; i <= n;i++) {
+      a[i] = 1;
+    }
+}
+void eratosthenes_sieve(int n, int a[]) {
+    for (int i = 2; i*i <= n; i++) {
+        if(a[i]) {
+        for(int j = i*i; j<= n; j+=i){
+          a[j] =0;
+        }
+      }
+    }
+}   
+void output(int n, int a[]) {
+  printf("The prime numbers between 2 and %d are:\n",n);
+  for(int i = 2; i<=n; i++) {
+    if(a[i]) {
+      printf("%d\n",i);
+    }
+  }
+}
+int main() {
   int n = input_array_size();
   int a[10000];
   init_array(n,a);
   eratosthenes_sieve(n,a);
   output(n,a);
   return 0;
-}
-
-int input_array_size()
-{
-  int n;
-    printf("Enter the size of the array:");
-    scanf("%d",&n);
-    return n;
-}
-
-void init_array(int n, int a[n])
-{
-  for(int i = 2;i<=n;i++){
-    a[i] = 1;
-  }
-}
-
-void erotosthenes_sieve(int n, int a[n])
-{
-  for(int i =2;i*1 <=n;i++){
-    if(a[i]){
-      for(int j =i*i;j<=n;j+=i){
-        a[j] =0;
-      }
-    }
-  }
-}
-
-void output(int n, int a[n])
-{
-  printf("The prime numbers between 2 and %d are:\n",n);
-  for(int i=2; i<=n;i++){
-    if(a[i]){
-      printf("%d\n",i);
-    }
-  }
 }
