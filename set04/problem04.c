65e77832508b163d9ee25ae9fc38dd6ea05d7f38
Asha <hampana@Hampana.local>
2024-03-04 13:09:34 +0530

    test

---
+++ set04/problem04.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+int input_degree();
+void input_coefficients(int n, float a[n]);
+float input_x();
+float evaluate_polynomial(int n, float a[n], float x);
+void output(int n, float a[n], float x, float result);
